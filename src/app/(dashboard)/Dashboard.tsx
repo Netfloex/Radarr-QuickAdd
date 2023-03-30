95c82a2cccc35ee33d32122a24f69d5d519b9611
Netfloex <38650595+Netfloex@users.noreply.github.com>
2023-03-30 15:06:21 +0200

    Add and retrieve search query from url

--- src/app/(dashboard)/Dashboard.tsx
+++ src/app/(dashboard)/Dashboard.tsx
@@ -1,7 +1,9 @@
 "use client"
 
 import Link from "next/link"
-import { useState } from "react"
+import { useSearchParams } from "next/navigation"
+import { useCallback, useState } from "react"
+import { useDebouncedCallback } from "use-debounce"
 
 import { Results } from "./Results"
 import { SearchField } from "./SearchField"
@@ -10,11 +12,26 @@ import { StatusCheck } from "./StatusCheck"
 import type { FC } from "react"
 
 export const Dashboard: FC = () => {
-	const [query, setQuery] = useState("")
+	const params = useSearchParams()
+
+	const queryParam = params?.get("query") || ""
+
+	const [query, setQuery] = useState(queryParam)
+
+	const onSearchChange = useCallback((value: string) => {
+		const newUrl = !value ? location.href.split("?")[0] : `?query=${value}`
+		history.pushState({ ...history.state, as: newUrl, newUrl }, "", newUrl)
+		setQuery(value)
+	}, [])
+
+	const onSearchChangeDebounced = useDebouncedCallback(onSearchChange, 400)
 
 	return (
 		<div className="container">
-			<SearchField query={query} setQuery={setQuery} />
+			<SearchField
+				defaultValue={queryParam}
+				onChange={onSearchChangeDebounced}
+			/>
 			<Link href="/settings">Settings</Link>
 			<StatusCheck />
 			<Results query={query} />
--- src/app/(dashboard)/SearchField.tsx
+++ src/app/(dashboard)/SearchField.tsx
@@ -2,20 +2,20 @@ import styles from "./SearchField.module.scss"
 
 import { Input } from "@components/joy"
 
-import type { Dispatch, FC, SetStateAction } from "react"
+import type { FC } from "react"
 
 export const SearchField: FC<{
-	query: string
-	setQuery: Dispatch<SetStateAction<string>>
-}> = ({ query, setQuery }) => {
+	defaultValue: string
+	onChange: (value: string) => void
+}> = ({ defaultValue, onChange }) => {
 	return (
 		<div className={styles.searchField}>
 			<Input
 				type="text"
-				value={query}
 				autoFocus
 				placeholder="Enter a movie"
-				onChange={(ev): void => setQuery(ev.target.value)}
+				onChange={(ev): void => onChange(ev.target.value)}
+				defaultValue={defaultValue}
 			/>
 		</div>
 	)
